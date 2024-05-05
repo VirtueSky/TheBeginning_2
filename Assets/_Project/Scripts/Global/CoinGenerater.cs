45b1d80e2d80298be4e87e0fcf7b730a7146e01f
Wolf <ducnv.cntt0502@gmail.com>
2024-05-05 18:53:52 +0700

    update

---
+++ Assets/_Project/Scripts/Global/CoinGenerater.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using PrimeTween;
+using UnityEngine;
+using UnityEngine.Serialization;
+using VirtueSky.Core;
+using VirtueSky.ObjectPooling;
+using VirtueSky.Threading.Tasks;
+using Random = UnityEngine.Random;
+
+namespace Base.Global
+{
+    public class CoinGenerater : BaseMono
+    {
+        [SerializeField] private Vector3 from = Vector3.zero;
+        [SerializeField] private GameObject to;
+        [SerializeField] private int numberCoin;
+        [SerializeField] private int delay;
+        [SerializeField] private float durationNear;
+        [SerializeField] private float durationTarget;
+        [SerializeField] private Ease easeNear;
+        [SerializeField] private Ease easeTarget;
+        [SerializeField] private float scale = 1;
+        [SerializeField] private float offsetNear = 1;
+        [SerializeField] private GameObject iconCoinPrefab;
+
+        private System.Action moveOneCoinDone;
+        private bool isScaleIconTo = false;
+
+        private List<GameObject> coinsActive = new List<GameObject>();
+
+        public void SetFrom(Vector3 from)
+        {
+            this.from = from;
+        }
+
+        public void SetToGameObject(GameObject to)
+        {
+            this.to = to;
+        }
+
+        private void Start()
+        {
+        }
+
+        public async void GenerateCoin(Action moveOneCoinDone, Action moveAllCoinDone,
+            GameObject to = null,
+            int numberCoin = -1)
+        {
+            isScaleIconTo = false;
+            this.moveOneCoinDone = moveOneCoinDone;
+            //this.moveAllCoinDone = moveAllCoinDone;
+            this.to = to == null ? this.to : to;
+            this.numberCoin = numberCoin < 0 ? this.numberCoin : numberCoin;
+
+            for (int i = 0; i < this.numberCoin; i++)
+            {
+                await UniTask.Delay(Random.Range(0, delay));
+                GameObject coin = PoolManager.Instance.Spawn(iconCoinPrefab, transform);
+                coin.transform.localScale = Vector3.one * scale;
+                coinsActive.Add(coin);
+                coin.transform.position = from;
+                MoveCoin(coin, moveAllCoinDone);
+                // if (i == numberCoin - 1)
+                // {
+                //     Observer.CoinMove?.Invoke();
+                // }
+            }
+        }
+
+        private void MoveCoin(GameObject coin, Action moveAllCoinDone)
+        {
+            MoveToTarget(coin, () =>
+            {
+                coinsActive.Remove(coin);
+                PoolManager.Instance.DeSpawn(coin);
+                if (!isScaleIconTo)
+                {
+                    isScaleIconTo = true;
+                    ScaleIconTo();
+                }
+
+                moveOneCoinDone?.Invoke();
+                if (coinsActive.Count == 0)
+                {
+                    moveAllCoinDone?.Invoke();
+
+                    from = Vector3.zero;
+                }
+            });
+        }
+
+
+        private void MoveToTarget(GameObject coin, Action completed)
+        {
+            coin.transform
+                .DOMove(coin.transform.position + (Vector3)Random.insideUnitCircle * offsetNear,
+                    durationNear)
+                .SetEase(easeNear)
+                .OnComplete(
+                    () =>
+                    {
+                        coin.transform.DOMove(to.transform.position, durationTarget).SetEase(easeTarget)
+                            .OnComplete(completed);
+                    });
+        }
+
+        public void SetNumberCoin(int _numberCoin)
+        {
+            numberCoin = _numberCoin;
+        }
+
+        private void ScaleIconTo()
+        {
+            Vector3 currentScale = Vector3.one;
+            Vector3 nextScale = currentScale + new Vector3(.1f, .1f, .1f);
+            to.transform.DOScale(nextScale, durationTarget).SetEase(Ease.OutBack)
+                .OnComplete((() => { to.transform.DOScale(currentScale, durationTarget / 2).SetEase(Ease.InBack); }));
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Global/CoinUpdater.cs
@@ -0,0 +1,92 @@
+using Base.Data;
+using PrimeTween;
+using TMPro;
+using UnityEngine;
+using VirtueSky.Audio;
+using VirtueSky.Core;
+
+namespace Base.Global
+{
+    public class CoinUpdater : BaseMono
+    {
+        public TextMeshProUGUI CurrencyAmountText;
+        public int StepCount = 10;
+        public float DelayTime = .01f;
+        public CoinGenerater coinGenerate;
+        [SerializeField] private SoundData soundCoinMove;
+
+
+        private int _currentCoin;
+
+        private void OnEnable()
+        {
+            Observer.CoinTotalChanged += UpdateCoinAmountText;
+            CurrencyAmountText.text = UserData.CoinTotal.ToString();
+            SaveCurrentCoin();
+        }
+
+        private void OnDisable()
+        {
+            Observer.CoinTotalChanged -= UpdateCoinAmountText;
+        }
+
+        private void SaveCurrentCoin()
+        {
+            _currentCoin = UserData.CoinTotal;
+        }
+
+        public void UpdateCoinAmountText()
+        {
+            if (UserData.CoinTotal > _currentCoin)
+            {
+                IncreaseCoin();
+            }
+            else
+            {
+                DecreaseCoin();
+            }
+        }
+
+        private void IncreaseCoin()
+        {
+            bool isFirstMove = false;
+            coinGenerate.GenerateCoin(() =>
+            {
+                if (!isFirstMove)
+                {
+                    isFirstMove = true;
+                    AudioManager.Instance.PlaySfx(soundCoinMove);
+                    int currentCurrencyAmount = int.Parse(CurrencyAmountText.text);
+                    int nextAmount = (UserData.CoinTotal - currentCurrencyAmount) / StepCount;
+                    int step = StepCount;
+                    CoinTextCount(currentCurrencyAmount, nextAmount, step);
+                }
+            }, () => { SaveCurrentCoin(); });
+        }
+
+        private void DecreaseCoin()
+        {
+            int currentCurrencyAmount = int.Parse(CurrencyAmountText.text);
+            int nextAmount = (UserData.CoinTotal - currentCurrencyAmount) / StepCount;
+            int step = StepCount;
+            CoinTextCount(currentCurrencyAmount, nextAmount, step);
+            SaveCurrentCoin();
+        }
+
+        private void CoinTextCount(int currentCurrencyValue, int nextAmountValue, int stepCount)
+        {
+            if (stepCount == 0)
+            {
+                CurrencyAmountText.text = UserData.CoinTotal.ToString();
+                return;
+            }
+
+            int totalValue = (currentCurrencyValue + nextAmountValue);
+            DOTween.Sequence().AppendInterval(DelayTime).SetUpdate(isIndependentUpdate: true).AppendCallback(() =>
+                {
+                    CurrencyAmountText.text = totalValue.ToString();
+                })
+                .AppendCallback(() => { CoinTextCount(totalValue, nextAmountValue, stepCount - 1); });
+        }
+    }
+}
